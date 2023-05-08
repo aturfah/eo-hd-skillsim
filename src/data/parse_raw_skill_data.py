f5a2d2bc7e5b7110cefaf60526f0bf42500892c9
Ali Turfah <aturfah@gmail.com>
2023-05-08 09:35:28 -0400

    Get mapping between skillsim &Shiri's files going

--- src/data/parse_raw_skill_data.py
+++ src/data/parse_raw_skill_data.py
@@ -1,10 +1,14 @@
+from pprint import pprint
+import json
+
 if __name__ == "__main__":
     with open("EO3 Skill Data - Skills.csv", "r") as in_file:
       raw_skill_rows = in_file.readlines()
     
     raw_skill_rows = [x.strip().split(",") for x in raw_skill_rows]
     
-    parsed_skills = []
+    ## Parse Shiri's skill file
+    parsed_skills = {}
     for skill_datum in raw_skill_rows:
         # print(skill_datum, end="\n\n")
         name = skill_datum[0]
@@ -12,16 +16,52 @@ if __name__ == "__main__":
         offset = 24
         max_len = len(skill_datum)
 
-        if name != "Royal Veil":
-           continue
-
+        skill_data_levels = []
         while offset + 11 < max_len:
            header_idx = offset + 1
            values_range = [x for x in range((header_idx+1), (header_idx+11))]
-           print(skill_datum[header_idx],
-                 [skill_datum[x] for x in values_range])
+           
+           
+           skill_data_levels.append({
+              "header": header_idx,
+              "levels": [skill_datum[x] for x in values_range]
+           })
            offset = offset + 11
 
-        # print(name, max_level, skill_datum[offset + 1], )
-        # print(max_len, offset, max_len - offset)
-        raise RuntimeError("AAAHH")
+
+        if name not in parsed_skills.keys():
+            parsed_skills[name] = {
+                "name": name,
+                "max_level": max_level,
+                "data": skill_data_levels
+            }
+        else:
+           parsed_skills[name]["data"].extend(skill_data_levels)
+
+    ## Now we get the old skillsim data to match our skillsim data
+    with open("skills.json", "r", encoding="utf8") as in_file:
+        old_class_skills = json.load(in_file)
+    
+    with open("levels.json", "r", encoding="utf8") as in_file:
+        old_skill_levels = json.load(in_file)
+
+    old_key_name_map = {}
+    for class_key in old_class_skills:
+       for skill_key in old_class_skills[class_key]:
+            skill_datum = old_class_skills[class_key][skill_key]
+            eng_name = skill_datum["name_en"]
+            matches = eng_name in parsed_skills.keys()
+
+            old_key_name_map[skill_key] = skill_datum["name_en"]
+
+        
+
+            if not matches:
+                print(skill_key, eng_name, matches)
+
+    X = {}
+    
+
+    # pprint(old_class_skills)
+    # print("\n\n")
+    # pprint(old_skill_levels)
